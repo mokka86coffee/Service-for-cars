481502c7f3df23e1da25cc6f990745b6126d87fd
mokka86coffee <mokka86coffee@gmail.com>
2019-04-18 10:35:11 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import { connect } from 'react-redux';
 import Carservice from './Components/Carservice';
 
 class App extends React.Component {
+    state = {
+        a:1
+    }
+
+    fn = () => this.setState( {a: this.state.a+1}, function() {console.log('this.state - ', this.state)} )
+
     render() {
         return (
             <React.Fragment>
+                <input type="text" onClick={this.fn} />
                 <Carservice />
             </React.Fragment>
         )
