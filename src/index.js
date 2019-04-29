68a381e0ee2009482b4e75172d529fc78cf29014
mokka86coffee <mokka86coffee@gmail.com>
2019-04-29 07:32:40 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@
     }`;
     document.body.appendChild(style);
 }
+
 const touchStart = e => console.log('touchStart', e.touches[0]);
 const touchEnd  = e => console.log('touchEnd',e.touches[0]);
 const touchMove = e => console.log('touchMove', e.touches[0]);
@@ -44,37 +45,67 @@ div.addEventListener( 'touchstart', touchStart );
 div.addEventListener( 'touchend', touchEnd );
 div.addEventListener( 'touchmove', touchMove );
 
+import { observer } from 'mobx-react'
+import mobx from 'mobx'
+import React from "react"
+import ReactDOM from "react-dom"
+@observer
+class TodoList extends React.Component {
+  render() {
+    const store = thi   s.props.store; 
+    return (
+      <div>
+        { store.report }
+        <ul>
+        { store.todos.map(
+          (todo, idx) => <TodoView todo={ todo } key={ todo.id } />
+        ) }
+        </ul>
+        { store.pendingRequests > 0 ? <marquee>Loading...</marquee> : null }
+        <button onClick={ this.onNewTodo }>New Todo</button>
+        <small> (double-click a todo to edit)</small>
+        <RenderCounter />
+      </div>
+    );
+  }
 
-class ObservableTodoStore {
-    @observable todos = [];
-    @observable pendingRequests = 0;
-
-    constructor() {
-        mobx.autorun(() => console.log(this.report));
-    }
-
-    @computed get completedTodosCount() {
-        return this.todos.filter(
-            todo => todo.completed === true
-        ).length;
-    }
-
-    @computed get report() {
-        if (this.todos.length === 0)
-            return "<none>";
-
-        return `Next todo: "${this.todos[0].task}". ` + 
-            `Progress: ${this.completedTodosCount}/${this.todos.length}`; 
-    }
-
-    addTodo(task) {
-        this.todos.push({ 
-            id: Date.now(),
-            task: task,
-            completed: false,
-            assignee: null
-        });
-    }
+  onNewTodo = () => { 
+    this.props.store.addTodo(prompt('Enter a new todo:','coffee plz')); 
+  } 
 }
 
-const observableTodoStore = new ObservableTodoStore();
+class TodoView extends React.Component {
+  render() {
+    const todo = this.props.todo;
+    return (
+      <li onDoubleClick={ this.onRename }>
+        <input 
+          type='checkbox'
+          checked={ todo.completed }
+          onChange={ this.onToggleCompleted } 
+        />
+        { todo.task }
+        { todo.assignee 
+          ? <small>{ todo.assignee.name }</small> 
+          : null
+        }
+        <RenderCounter />
+      </li>
+    ); 
+  }
+
+  onToggleCompleted = () => {
+    const todo = this.props.todo;
+    todo.completed = !todo.completed;
+  }
+
+  onRename = () => {
+    const todo = this.props.todo;
+    todo.task = prompt('Task name', todo.task) || ""; 
+  } 
+}
+
+ReactDOM.render(
+  <TodoList store={ observableTodoStore } />, 
+  document.getElementById('reactjs-app')
+);
